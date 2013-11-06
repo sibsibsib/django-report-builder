cf8ba0739cc3b1ecd80439bd7691849b7354814a
Andrew Sibley <andrew.sibley@gmail.com>
2013-11-06 17:11:46 -0500

    temporarily fixed star ajax button in report change list

--- report_builder/static/report_builder/js/report_list.js
+++ report_builder/static/report_builder/js/report_list.js
@@ -2,40 +2,37 @@
 //    <static>/report_builder/js/report_form.js
 
 // initialize path prefix.
-path_prefix = ""
+var path_prefix = "";
 
 // get current path
-current_path =  window.location.pathname;
+var current_path = window.location.pathname;
 
 // where is "/report_builder"?
-root_index = current_path.indexOf( "/report_builder" );
+var root_index = current_path.indexOf( "/report_builder" );
 
 // other than 0?
-if ( root_index > 0 )
-{
-
+if ( root_index > 0 ) {
     // yes.  Get all text from start to that location, store it as path_prefix.
     path_prefix = current_path.substring( 0, root_index );
-
 }
-else
-{
-
+else {
     // no - set path_prefix to "".
     path_prefix = "";
-
 }
 
 
-function ajax_add_star(event, url) {    
-    $.post(  
+function ajax_add_star(event, url) {
+    $.post(
         url,
-	{},
+        {
+            csrfmiddlewaretoken: _csrf     // TODO do this properly
+        },
         function(data){
-	    if (data == 'True' ) {
-		$(event).html('<img style="width: 26px; margin: -6px;" src="/static/report_builder/img/star.png">');
-	    } else {
-		$(event).html('<img style="width: 26px; margin: -6px;" src="/static/report_builder/img/unstar.png">');
-	    }
+        if (data === 'True' ) {
+            $(event).html('<img style="width: 26px; margin: -6px;" src="/static/report_builder/img/star.png">');
+        } else {
+            $(event).html('<img style="width: 26px; margin: -6px;" src="/static/report_builder/img/unstar.png">');
         }
-    );}
+        }
+    );
+}
